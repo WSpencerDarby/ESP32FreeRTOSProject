dbc83ad1a2b9949479a96874d7eca099409f56cd
William Darby <wdarby@purdue.edu>
2026-04-01 19:03:29 -0700

    Development Enviroment

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+#define LED 13
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED, HIGH);
+  Serial.println("LED is on");
+  delay(1000);
+  digitalWrite(LED, LOW);
+  Serial.println("LED is off");
+  delay(1000);
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
